734df71c0a1cf785595e12ff87e39a069174be0f
SumitSingh69 <issomeonein1234@gmail.com>
2025-09-24 12:00:48 +0530

    added persistent connection

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
 function Body() {
+  const dispatch = useDispatch();
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true, // its a protected route so we need to send our cookies as well to the backend
+      });
+
+      console.log(res.data);
+      dispatch(addUser(res.data));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    fetchUser();
+  }, []); //run after every page reload
   return (
     <div>
       <Navbar />
