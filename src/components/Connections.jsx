5b018e36e45e5f868b6f67b16e8fb9cdba022b4d
SumitSingh69 <issomeonein1234@gmail.com>
2025-09-25 19:01:14 +0530

    created conectionSlice and added it to appStore

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addConnection } from "../utils/connectionSlice";
 function Connections() {
   const fetchConnections = async () => {
+    const dispatch = useDispatch();
     try {
       const res = await axios.get(BASE_URL + "/user/connections", {
         withCredentials: true,
       });
       console.log(res.data.allConnections);
+      dispatch(addConnection(res.data.allConnections));
     } catch (err) {
       console.log(err);
     }
--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import feedReducer from "./feedSlice";
+import connectionReducers from "./connectionSlice";
 const appstore = configureStore({
   reducer: {
     user: userReducer,
     feed: feedReducer,
+    connections: connectionReducers,
   },
 });
 
